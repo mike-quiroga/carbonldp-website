f37f0b6bd3c73321e72966bd8c54a3852c855c41
Miguel Aragón <miguel@araco.mx>
2015-03-18 12:24:40 -0600

    ACL Inheritance

--- src/main/java/com/carbonldp/authorization/acl/AbstractACLPermissionVoter.java
+++ src/main/java/com/carbonldp/authorization/acl/AbstractACLPermissionVoter.java
@@ -1,21 +1,35 @@
 package com.carbonldp.authorization.acl;
 
 import com.carbonldp.AbstractComponent;
+import com.carbonldp.rdf.RDFNodeEnum;
+import com.carbonldp.utils.ACLUtil;
+import org.openrdf.model.URI;
 import org.springframework.util.Assert;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public abstract class AbstractACLPermissionVoter extends AbstractComponent {
 	protected ACLRepository aclRepository;
 
-	public AbstractACLPermissionVoter() {
-
-	}
-
-	public AbstractACLPermissionVoter( ACLRepository aclRepository ) {
-		setACLRepository( aclRepository );
-	}
-
 	public void setACLRepository( ACLRepository aclRepository ) {
 		Assert.notNull( aclRepository );
 		this.aclRepository = aclRepository;
+	}
+
+	protected Map<ACEDescription.Permission, Set<ACE>> getRelatedACEs( ACL acl, Map<RDFNodeEnum, Set<URI>> subjects, Set<ACEDescription.Permission> permissions ) {
+		Map<ACEDescription.Permission, Set<ACE>> permissionsACEs = new HashMap<>();
+		Set<ACE> aces = ACEFactory.get( acl, acl.getACEntries() );
+		for ( ACE ace : aces ) {
+			if ( ! ACLUtil.aceRefersToSubjects( ace, subjects ) ) continue;
+			for ( ACEDescription.Permission permission : permissions ) {
+				if ( ! ace.getPermissions().contains( permission ) ) continue;
+				if ( ! permissionsACEs.containsKey( permission ) ) permissionsACEs.put( permission, new HashSet<>() );
+				permissionsACEs.get( permission ).add( ace );
+			}
+		}
+		return permissionsACEs;
 	}
 }
--- src/main/java/com/carbonldp/authorization/acl/InheritanceACLPermissionVoter.java
+++ src/main/java/com/carbonldp/authorization/acl/InheritanceACLPermissionVoter.java
@@ -5,26 +5,44 @@ import com.carbonldp.apps.App;
 import com.carbonldp.apps.context.AppContextHolder;
 import com.carbonldp.rdf.RDFNodeEnum;
 import com.carbonldp.utils.ACLUtil;
+import com.carbonldp.web.exceptions.NotImplementedException;
 import org.openrdf.model.URI;
 import org.openrdf.model.impl.URIImpl;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class InheritanceACLPermissionVoter extends AbstractACLPermissionVoter implements ACLPermissionVoter {
-	private URI topParentURI;
-	private URI platformsRootContainerURI;
 
 	@Override
 	public Vote vote( Map<RDFNodeEnum, Set<URI>> subjects, Set<ACEDescription.Permission> permissions, URI objectURI ) {
+		Set<ACEDescription.Permission> permissionsToGrant = new HashSet<>();
+		permissionsToGrant.addAll( permissions );
+
 		URI topParentURI = getTopParentURI();
 		List<URI> parentURIs = ACLUtil.getParentURIs( objectURI, topParentURI );
 		for ( URI parentURI : parentURIs ) {
 			ACL parentACL = aclRepository.getResourceACL( parentURI );
-			// TODO: Evaluate ACL
+			if ( parentACL == null || parentACL.isEmpty() ) continue;
+
+			Map<ACEDescription.Permission, Set<ACE>> permissionsACEs = getRelatedACEs( parentACL, subjects, permissionsToGrant );
+			if ( permissionsACEs.isEmpty() ) continue;
+
+			for ( ACEDescription.Permission acePermission : permissionsACEs.keySet() ) {
+				Set<ACE> permissionACEs = permissionsACEs.get( acePermission );
+				if ( permissionACEs.size() != 1 ) throw new IllegalStateException( new NotImplementedException( "Subject class priority hasn't been implemented." ) );
+				ACE permissionACE = permissionACEs.iterator().next();
+				if ( ! permissionACE.isGranting() ) return Vote.DENY;
+				permissionsToGrant.remove( acePermission );
+			}
+
+			if ( permissionsToGrant.isEmpty() ) break;
 		}
-		return Vote.ABSTAIN;
+
+		if ( permissionsToGrant.isEmpty() ) return Vote.GRANT;
+		else return Vote.ABSTAIN;
 	}
 
 	private URI getTopParentURI() {
