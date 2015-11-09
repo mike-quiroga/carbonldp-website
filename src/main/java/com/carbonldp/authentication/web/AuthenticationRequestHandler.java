0bc67374739bfdfa64678c559e926b35d9a50dd8
Nestor Estrada <nestor.estrada@base22.com>
2015-11-09 10:38:41 -0600

    Change to JSON Web Token for token authentication

--- src/main/java/com/carbonldp/authentication/web/AuthenticationRequestHandler.java
+++ src/main/java/com/carbonldp/authentication/web/AuthenticationRequestHandler.java
@@ -6,17 +6,21 @@ import com.carbonldp.authentication.TokenRepresentationDescription;
 import com.carbonldp.ldp.web.AbstractLDPRequestHandler;
 import com.carbonldp.rdf.RDFResourceDescription;
 import com.carbonldp.web.RequestHandler;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.crypto.MacProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.token.Token;
 import org.springframework.security.core.token.TokenService;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.security.Key;
 
 /**
  * @author NestorVenegas
@@ -32,22 +36,26 @@ public class AuthenticationRequestHandler extends AbstractLDPRequestHandler {
 	public ResponseEntity<Object> handleRequest( HttpServletRequest request, HttpServletResponse response ) {
 		setUp( request, response );
 
+		SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		if ( ! ( authentication instanceof AgentAuthenticationToken ) ) throw new RuntimeException( "authentication is not an instance of AgentAuthenticationToken" );
 		AgentAuthenticationToken agentToken = (AgentAuthenticationToken) authentication;
-		Token token = tokenService.allocateToken( agentToken.getAgent().getSubject().stringValue() );
+		Key key = MacProvider.generateKey();
 
-		TokenRepresentation tokenRepresentation = getTokenRepresentation( token );
+		JwtBuilder builder = Jwts.builder()
+								 .setSubject( agentToken.getAgent().getSubject().stringValue() )
+								 .signWith( signatureAlgorithm, key );
 
+		TokenRepresentation tokenRepresentation = getTokenRepresentation( builder.compact() );
 		return new ResponseEntity<>( tokenRepresentation, HttpStatus.OK );
 
 	}
 
-	private TokenRepresentation getTokenRepresentation( Token token ) {
+	private TokenRepresentation getTokenRepresentation( String token ) {
 		TokenRepresentation tokenRepresentation = new TokenRepresentation();
 		tokenRepresentation.addType( TokenRepresentationDescription.Resource.CLASS.getURI() );
 		tokenRepresentation.addType( RDFResourceDescription.Resource.VOLATILE.getURI() );
-		tokenRepresentation.setTokenKey( token.getKey() );
+		tokenRepresentation.setTokenKey( token );
 		return tokenRepresentation;
 	}
 
