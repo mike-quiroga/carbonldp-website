a7d70cd6939aa64466bff6e990fa38321f09abd0
Nestor Estrada <nestor.estrada@base22.com>
2015-11-11 12:11:27 -0600

    Set an expiration date for the JSON Web Token

--- src/main/java/com/carbonldp/authentication/token/JWTAuthenticationEntryPoint.java
+++ src/main/java/com/carbonldp/authentication/token/JWTAuthenticationEntryPoint.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * @author Dev
+ * @author NestorVenegas
  * @since _version_
  */
 public class JWTAuthenticationEntryPoint implements AuthenticationEntryPoint {
--- src/main/java/com/carbonldp/authentication/web/AuthenticationRequestHandler.java
+++ src/main/java/com/carbonldp/authentication/web/AuthenticationRequestHandler.java
@@ -10,6 +10,7 @@ import com.carbonldp.web.RequestHandler;
 import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +24,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.xml.bind.DatatypeConverter;
 import java.security.Key;
+import java.util.Date;
 
 /**
  * @author NestorVenegas
@@ -38,6 +40,8 @@ public class AuthenticationRequestHandler extends AbstractLDPRequestHandler {
 	public ResponseEntity<Object> handleRequest( HttpServletRequest request, HttpServletResponse response ) {
 		setUp( request, response );
 
+		Date expTime = new Date( System.currentTimeMillis() + Vars.getInstance().getTokenExpTime() );
+
 		SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		if ( ! ( authentication instanceof AgentAuthenticationToken ) ) throw new RuntimeException( "authentication is not an instance of AgentAuthenticationToken" );
@@ -47,18 +51,20 @@ public class AuthenticationRequestHandler extends AbstractLDPRequestHandler {
 
 		JwtBuilder builder = Jwts.builder()
 								 .setSubject( agentToken.getAgent().getSubject().stringValue() )
-								 .signWith( signatureAlgorithm, signingKey );
+								 .signWith( signatureAlgorithm, signingKey )
+								 .setExpiration( expTime );
 
-		TokenRepresentation tokenRepresentation = getTokenRepresentation( builder.compact() );
+		TokenRepresentation tokenRepresentation = getTokenRepresentation( builder.compact(), expTime );
 		return new ResponseEntity<>( tokenRepresentation, HttpStatus.OK );
 
 	}
 
-	private TokenRepresentation getTokenRepresentation( String token ) {
+	private TokenRepresentation getTokenRepresentation( String token, Date expTime ) {
 		TokenRepresentation tokenRepresentation = new TokenRepresentation();
 		tokenRepresentation.addType( TokenRepresentationDescription.Resource.CLASS.getURI() );
 		tokenRepresentation.addType( RDFResourceDescription.Resource.VOLATILE.getURI() );
 		tokenRepresentation.setTokenKey( token );
+		tokenRepresentation.setExpTime( new DateTime( expTime ) );
 		return tokenRepresentation;
 	}
 
