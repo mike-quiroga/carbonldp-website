b136c6954394d2e87b115e9d0f67955de07cb41c
Nestor Estrada <nestor.estrada@base22.com>
2015-12-01 16:52:09 -0600

    Create handler and controller to add agents to an app role

--- src/main/java/com/carbonldp/apps/roles/web/AppRolesController.java
+++ src/main/java/com/carbonldp/apps/roles/web/AppRolesController.java
@@ -1,16 +1,38 @@
 package com.carbonldp.apps.roles.web;
 
+import com.carbonldp.descriptions.APIPreferences;
+import com.carbonldp.ldp.web.AbstractLDPController;
+import com.carbonldp.rdf.RDFDocument;
+import com.carbonldp.web.config.InteractionModel;
 import com.carbonldp.web.exceptions.NotImplementedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.PrimitiveIterator;
+
 @Controller
-@RequestMapping( value = "apps/*/roles/" )
-public class AppRolesController {
-	@RequestMapping( method = RequestMethod.POST )
+public class AppRolesController extends AbstractLDPController {
+
+	private AppRolesPUTAgentsHanlder appRolesPUTAgentsHanlder;
+
+	@RequestMapping( method = RequestMethod.POST, value = "apps/*/roles/" )
 	public void createAppRole() {
 		// TODO: Implement it
 		throw new NotImplementedException();
 	}
+
+	@RequestMapping( method = RequestMethod.PUT, value = "apps/*/roles/*/agents/" )
+	@InteractionModel( APIPreferences.InteractionModel.CONTAINER )
+	public ResponseEntity<Object> addAgentToRole( RDFDocument requestDocument, HttpServletRequest request, HttpServletResponse response ) {
+		return appRolesPUTAgentsHanlder.handleRequest( requestDocument, request, response );
+	}
+
+	@Autowired
+	public void setAppRolesPUTAgentsHanlder( AppRolesPUTAgentsHanlder appRolesPUTAgentsHanlder ) {this.appRolesPUTAgentsHanlder = appRolesPUTAgentsHanlder;}
+
 }
---
+++ src/main/java/com/carbonldp/apps/roles/web/AppRolesPUTAgentsHanlder.java
@@ -0,0 +1,32 @@
+package com.carbonldp.apps.roles.web;
+
+import com.carbonldp.exceptions.InvalidResourceException;
+import com.carbonldp.ldp.containers.AbstractPUTRequestHandler;
+import com.carbonldp.ldp.containers.AddMembersAction;
+import com.carbonldp.ldp.containers.AddMembersActionFactory;
+import com.carbonldp.ldp.containers.MembersAction;
+import com.carbonldp.models.Infraction;
+import com.carbonldp.web.RequestHandler;
+import org.openrdf.model.URI;
+
+import java.util.List;
+
+/**
+ * @author NestorEstrada
+ * @since _version_
+ */
+
+@RequestHandler
+public class AppRolesPUTAgentsHanlder extends AbstractPUTRequestHandler {
+
+	@Override
+	protected void executeAction( URI targetUri, AddMembersAction members ) {
+		validate( members );
+		appRoleService.addAgentMembers( targetUri, members.getMembers() );
+	}
+
+	protected void validate( MembersAction membersAction ) {
+		List<Infraction> infractions = AddMembersActionFactory.getInstance().validate( membersAction );
+		if ( ! infractions.isEmpty() ) throw new InvalidResourceException( infractions );
+	}
+}
