5ec9a913f7f206f0f47d796a96a618d5cb9c1b15
Jorge <jorge.espinosa@base22.com>
2016-02-23 15:29:52 -0600

    Create JobManager and JobConfig to administrate jobs queue

---
+++ src/main/java/com/carbonldp/config/JobConfig.java
@@ -0,0 +1,12 @@
+package com.carbonldp.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author JorgeEspinosa
+ * @since _version_
+ */
+@Configuration
+public class JobConfig {
+
+}
---
+++ src/main/java/com/carbonldp/jobs/JobManager.java
@@ -0,0 +1,8 @@
+package com.carbonldp.jobs;
+
+/**
+ * @author JorgeEspinosa
+ * @since _version_
+ */
+public class JobManager {
+}
