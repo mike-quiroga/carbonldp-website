00e7e86773618cd841878b1bc06dffb0f82b817d
Jorge <jorge.espinosa@base22.com>
2016-02-25 18:45:30 -0600

    Final version update 1.4.0: add bNode identifier

--- src/main/java/com/carbonldp/repository/updates/UpdateAction1o4o0.java
+++ src/main/java/com/carbonldp/repository/updates/UpdateAction1o4o0.java
@@ -5,15 +5,18 @@ import com.carbonldp.rdf.RDFBlankNodeDescription;
 import com.carbonldp.utils.ValueUtil;
 import org.openrdf.model.*;
 import org.openrdf.model.impl.ValueFactoryImpl;
+import org.openrdf.query.*;
 import org.openrdf.repository.RepositoryException;
-import org.openrdf.repository.RepositoryResult;
 
 import java.util.Set;
 import java.util.UUID;
 
+import static com.carbonldp.Consts.NEW_LINE;
+import static com.carbonldp.Consts.TAB;
+
 /**
  * @author JorgeEspinosa
- * @since _version_
+ * @since version
  */
 public class UpdateAction1o4o0 extends AbstractUpdateAction {
 
@@ -39,20 +42,32 @@ public class UpdateAction1o4o0 extends AbstractUpdateAction {
 		}
 	}
 
-	private void addBNodeIdentifier() throws RepositoryException {
+	private static final String getBNodesWithoutIDQuery;
 
-		RepositoryResult<Statement> statements = connectionFactory.getConnection().getStatements( null, null, null, true );
-		statements.enableDuplicateFilter();
-		while ( statements.hasNext() ) {
-			Statement statement = statements.next();
-			Resource subject = statement.getSubject();
-			if ( ! ( ValueUtil.isBNode( subject ) ) ) continue;
-			Resource context = statement.getContext();
-			BNode bNode = ValueUtil.getBNode( subject );
-			URI predicate = RDFBlankNodeDescription.Property.BNODE_IDENTIFIER.getURI();
-			if ( connectionFactory.getConnection().hasStatement( bNode, predicate, null, false, context ) ) continue;
-			Literal object = ValueFactoryImpl.getInstance().createLiteral( UUID.randomUUID().toString() );
-			connectionFactory.getConnection().add( bNode, predicate, object, context );
-		}
+	static {
+		getBNodesWithoutIDQuery = "" +
+				"SELECT DISTINCT ?s ?c" + NEW_LINE +
+				"WHERE {" + NEW_LINE +
+				TAB + "GRAPH ?c" + NEW_LINE +
+				TAB + TAB + "{?s ?p ?o}" + NEW_LINE +
+				TAB + "FILTER isBlank(?s)." + NEW_LINE +
+				TAB + "FILTER NOT EXISTS {?s <" + RDFBlankNodeDescription.Property.BNODE_IDENTIFIER.getURI().stringValue() + "> ?o}" + NEW_LINE +
+				"}";
+	}
+
+	private void addBNodeIdentifier() throws RepositoryException {
+		URI predicate = RDFBlankNodeDescription.Property.BNODE_IDENTIFIER.getURI();
+
+		sparqlTemplate.executeTupleQuery( getBNodesWithoutIDQuery, null, queryResult -> {
+			while ( queryResult.hasNext() ) {
+				Literal object = ValueFactoryImpl.getInstance().createLiteral( UUID.randomUUID().toString() );
+				BindingSet bindingSet = queryResult.next();
+				Value subject = bindingSet.getValue( "s" );
+				Value context = bindingSet.getValue( "c" );
+				connectionFactory.getConnection().add( ValueUtil.getBNode( subject ), predicate, object, ValueUtil.getURI( context ) );
+			}
+			return true;
+		} );
+
 	}
 }
