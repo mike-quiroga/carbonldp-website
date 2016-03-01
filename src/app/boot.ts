d77620e00e86e4c208f8e6224dc35bb69bf8cd47
Alvaro Contreras <alvaro9210@gmail.com>
2016-03-01 11:53:38 -0600

    Add credential validation to AppDev.

--- src/app/boot.ts
+++ src/app/boot.ts
@@ -2,7 +2,7 @@ import "zone.js";
 import "reflect-metadata";
 
 import { bootstrap } from "angular2/platform/browser";
-import { provide } from "angular2/core";
+import { provide, Injector } from "angular2/core";
 import { FORM_PROVIDERS } from "angular2/common";
 import { ROUTER_PROVIDERS, APP_BASE_HREF } from "angular2/router";
 import { HTTP_PROVIDERS } from "angular2/http";
@@ -17,6 +17,15 @@ import { BLOG_PROVIDERS } from 'app/blog/Blog';
 import { APP_DEV_PROVIDERS } from 'app/app-dev/AppDev';
 import { SIDEBAR_PROVIDERS } from 'app/app-dev/components/sidebar/Sidebar';
 
+
+let appInjectorRef:Injector;
+export const appInjector = ( injector?:Injector ):Injector => {
+	if ( injector ) {
+		appInjectorRef = injector;
+	}
+	return appInjectorRef;
+};
+
 const CARBON_PROVIDER = provide( Carbon, {
 	useFactory: () => {
 		let carbon = new Carbon();
@@ -30,11 +39,15 @@ bootstrap( AppComponent, [
 	ROUTER_PROVIDERS,
 	HTTP_PROVIDERS,
 
-	provide( APP_BASE_HREF, { useValue: "/carbon-website/src/" } ),
+	provide( APP_BASE_HREF, {useValue: "/carbon-website/src/"} ),
 
 	CARBON_PROVIDER,
 	CONTENT_PROVIDERS,
 	BLOG_PROVIDERS,
 	APP_DEV_PROVIDERS,
 	SIDEBAR_PROVIDERS
-] );
+] ).then(
+	( appRef:ComponentRef ) => {
+		appInjector( appRef.injector );
+	}
+);
