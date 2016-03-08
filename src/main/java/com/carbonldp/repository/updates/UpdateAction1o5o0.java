40a463cb9cc6d030ce519d46eb81ce8f0618aab1
Jorge <jorge.espinosa@base22.com>
2016-03-08 15:30:56 -0600

    Create update 1.5.0 to add backup and jobs containers to apps

---
+++ src/main/java/com/carbonldp/repository/updates/UpdateAction1o5o0.java
@@ -0,0 +1,38 @@
+package com.carbonldp.repository.updates;
+
+import com.carbonldp.apps.App;
+import com.carbonldp.authorization.acl.ACLDescription;
+import com.carbonldp.ldp.sources.RDFSourceDescription;
+
+import java.util.Set;
+
+import static com.carbonldp.Consts.NEW_LINE;
+import static com.carbonldp.Consts.TAB;
+
+/**
+ * @author JorgeEspinosa
+ * @since 0.27.8-ALPHA
+ */
+public class UpdateAction1o5o0 extends AbstractUpdateAction {
+
+	final String updateACLTripleQuery = "" +
+		"INSERT {" + NEW_LINE +
+		TAB + "GRAPH ?target {" + NEW_LINE +
+		TAB + TAB + "?target <" + RDFSourceDescription.Property.ACCESS_CONTROL_LIST.getURI().stringValue() + "> ?acl." + NEW_LINE +
+		TAB + "}." + NEW_LINE +
+		"} WHERE {" + NEW_LINE +
+		TAB + "GRAPH ?acl {" + NEW_LINE +
+		TAB + TAB + "?acl <" + ACLDescription.Property.ACCESS_TO.getURI().stringValue() + "> ?target." + NEW_LINE +
+		TAB + "}." + NEW_LINE +
+		"}";
+
+	@Override
+	public void execute() throws Exception {
+		transactionWrapper.runInPlatformContext( () -> sparqlTemplate.executeUpdate( updateACLTripleQuery, null ) );
+		Set<App> apps = getAllApps();
+		for ( App app : apps ) {
+			transactionWrapper.runInAppContext( app, () -> sparqlTemplate.executeUpdate( updateACLTripleQuery, null ) );
+		}
+	}
+}
+
