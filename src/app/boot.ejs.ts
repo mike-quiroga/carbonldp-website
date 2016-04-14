41f1dab38715e8a97b0858ed26dcff84d02ee791
Alvaro Contreras <alvaro9210@gmail.com>
2016-04-14 09:03:17 -0500

    Add changes to changelog and remove extendObjectSchema for description from boot

--- src/app/boot.ejs.ts
+++ src/app/boot.ejs.ts
@@ -4,31 +4,25 @@
 import "zone.js";
 import "reflect-metadata";
 
-import { bootstrap } from "angular2/platform/browser";
-import { provide, enableProdMode, Provider, ComponentRef } from "angular2/core";
-import { FORM_PROVIDERS } from "angular2/common";
-import { ROUTER_PROVIDERS, APP_BASE_HREF } from "angular2/router";
-import { HTTP_PROVIDERS } from "angular2/http";
+import {bootstrap} from "angular2/platform/browser";
+import {provide, enableProdMode, Provider, ComponentRef} from "angular2/core";
+import {FORM_PROVIDERS} from "angular2/common";
+import {ROUTER_PROVIDERS, APP_BASE_HREF} from "angular2/router";
+import {HTTP_PROVIDERS} from "angular2/http";
 
-import { appInjector, activeContext, CARBON_PROVIDERS } from "angular2-carbonldp/boot";
-import { CARBON_SERVICES_PROVIDERS } from "angular2-carbonldp/services";
+import {appInjector, activeContext, CARBON_PROVIDERS} from "angular2-carbonldp/boot";
+import {CARBON_SERVICES_PROVIDERS} from "angular2-carbonldp/services";
 
 import Carbon from "carbonldp/Carbon";
 
 import AppComponent from "app/AppComponent";
 
-import { BLOG_PROVIDERS } from "app/website/blog/Blog";
-import { APP_DEV_PROVIDERS } from "app/app-dev/AppDev";
+import {BLOG_PROVIDERS} from "app/website/blog/Blog";
+import {APP_DEV_PROVIDERS} from "app/app-dev/AppDev";
 
 
 let carbon:Carbon = new Carbon();
 carbon.setSetting( "domain", "<%- carbon.domain %>" );
-carbon.extendObjectSchema( Carbon.App.RDF_CLASS, {
-	"description": {
-		"@id": "http://example.com/ns#description",
-		"@type:": Carbon.NS.XSD.DataType.string,
-	},
-} );
 activeContext.initialize( carbon );
 
 let providers:Provider[] = [];
@@ -36,7 +30,7 @@ providers = providers
 	.concat( CARBON_PROVIDERS )
 	.concat( CARBON_SERVICES_PROVIDERS );
 
-if( "<%- debug %>" === "false" ) enableProdMode();
+if ( "<%- debug %>" === "false" ) enableProdMode();
 
 bootstrap( AppComponent, [
 	FORM_PROVIDERS,
