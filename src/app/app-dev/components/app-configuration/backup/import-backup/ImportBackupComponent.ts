9caab06e5b4d9fd537630dbcd346dbf1c5aec0ba
Alvaro Contreras <alvaro9210@gmail.com>
2016-05-23 17:31:30 -0500

    Fill Backups dropdown of ImportBackup component from available backups.

--- src/app/app-dev/components/app-configuration/backup/import-backup/ImportBackupComponent.ts
+++ src/app/app-dev/components/app-configuration/backup/import-backup/ImportBackupComponent.ts
@@ -4,7 +4,12 @@ import { CORE_DIRECTIVES, FormBuilder, ControlGroup, AbstractControl, Control, V
 import $ from "jquery";
 import "semantic-ui/semantic";
 
+import Carbon from "carbonldp/Carbon";
 import * as App from "carbonldp/App";
+import * as Response from "carbonldp/HTTP/Response";
+import * as PersistedDocument from "carbonldp/PersistedDocument";
+
+import BackupsService from "./../BackupsService";
 
 import template from "./template.html!";
 // import "./style.css!";
@@ -20,6 +25,7 @@ export default class ImportBackupComponent {
 	element:ElementRef;
 	$element:JQuery;
 	$importForm:JQuery;
+	$backups:Jquery;
 
 	formBuilder:FormBuilder;
 	importForm:ControlGroup;
@@ -29,13 +35,19 @@ export default class ImportBackupComponent {
 	backup:AbstractControl;
 	fileBackup:AbstractControl;
 
-	constructor( element:ElementRef, formBuilder:FormBuilder ) {
+	backups:PersistedDocument.Class[];
+	backupsService:BackupsService;
+	@Input() appContext:App.Context;
+
+	constructor( element:ElementRef, formBuilder:FormBuilder, backupsService:BackupsService ) {
 		this.element = element;
 		this.formBuilder = formBuilder;
+		this.backupsService = backupsService;
 	}
 
 	ngOnInit():void {
 		this.$element = $( this.element.nativeElement );
+		this.$backups = this.$element.find( "select.backups.list" );
 		this.$importForm = this.$element.find( "form.importForm" );
 		this.importForm = this.formBuilder.group( {
 			uriGroup: this.formBuilder.group( {
@@ -48,10 +60,29 @@ export default class ImportBackupComponent {
 		} );
 		this.uriGroup = <ControlGroup>this.importForm.controls[ "uriGroup" ];
 		this.fileGroup = <ControlGroup>this.importForm.controls[ "fileGroup" ];
-		this.uri = this.importForm.controls[ "uri" ];
-		this.backup = this.importForm.controls[ "backup" ];
+		this.uri = this.uriGroup.controls[ "uri" ];
+		this.backup = this.uriGroup.controls[ "backup" ];
 		this.fileBackup = this.importForm.controls[ "fileBackup" ];
 		// console.log( this.importForm );
+		this.getBackups();
+		this.initializeDropdowns();
+	}
+
+	initializeDropdowns():void {
+		this.$backups.dropdown( {
+			onChange: this.onSelectBackup.bind( this ),
+		} );
+	}
+
+	onSelectBackup( value:string, text:string, option:JQuery ):void {
+		(<Control> this.uri).updateValue( value );
+		this.uri.updateValueAndValidity();
+	}
+
+	getBackups():void {
+		this.backupsService.getAll( this.appContext ).then( ( [backups, response]:[PersistedDocument.Class[], Response.Class] )=> {
+			this.backups = backups;
+		} )
 	}
 
 	uriGroupValidator( corsGroup:ControlGroup ):any {
