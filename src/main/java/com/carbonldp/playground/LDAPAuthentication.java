a5087601a1e3cb20ac4985c1bb3ecb02fdb8f441
Jorge <jorge.espinosa@base22.com>
2016-05-23 16:31:44 -0600

    Modify the playground

--- src/main/java/com/carbonldp/playground/LDAPAuthentication.java
+++ src/main/java/com/carbonldp/playground/LDAPAuthentication.java
@@ -1,9 +1,12 @@
 package com.carbonldp.playground;
 
+import com.carbonldp.agents.LDAPAgent;
 import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.ldap.core.support.LdapContextSource;
 import org.springframework.ldap.filter.AndFilter;
 import org.springframework.ldap.filter.EqualsFilter;
+
+import java.util.List;
 
 /**
  * @author JorgeEspinosa
@@ -12,8 +15,8 @@ import org.springframework.ldap.filter.EqualsFilter;
 public class LDAPAuthentication {
 	public static void main( String[] args ) {
 		String url = "ldap://localhost:10389";
-		String base = "dc=Apache2,dc=Org2";
-		String userDn = "uid=admin,ou=system ";
+		String base = "dc=example,dc=com";
+		String userDn = "uid=admin,ou=system";
 		String password = "secret";
 		try {
 			LdapContextSource ldapContextSource = new LdapContextSource();
@@ -25,14 +28,13 @@ public class LDAPAuthentication {
 			LdapTemplate ldapTemplate = new LdapTemplate( ldapContextSource );
 
 			AndFilter filter = new AndFilter();
-//			filter.and( new EqualsFilter( "dc", "Apache2" ) );
-			filter.and( new EqualsFilter( "dc", "Apache2" ) );
-			boolean exists = ldapTemplate.authenticate( "", filter.toString(), "" );
-
+//			filter.and( new EqualsFilter( "objectClass", "person" ) );
+			filter.and( new EqualsFilter( "uid", "einstein" ) );
+			boolean exists = ldapTemplate.authenticate( "", filter.toString(), "elMismisimoAlbertEinstein" );
 
 			ContactAttributeMapperJSON mapperJSON = new ContactAttributeMapperJSON();
-//			List<LDAPAgent> list = ldapTemplate.search( "", filter.encode(), mapperJSON );
-//			System.out.print( list.toString() );
+			List<LDAPAgent> list = ldapTemplate.search( "", filter.encode(), mapperJSON );
+			System.out.print( list.toString() );
 		} catch ( Exception e ) {
 			e.printStackTrace();
 		}
