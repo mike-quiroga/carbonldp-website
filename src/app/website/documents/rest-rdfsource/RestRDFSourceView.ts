e495298dae985e6108b9d49f37f2cc03774c9c73
Cody Burleson <cody.burleson@base22.com>
2016-05-25 09:59:48 -0500

    Update my new document components with new angular import statements.

--- src/app/website/documents/rest-rdfsource/RestRDFSourceView.ts
+++ src/app/website/documents/rest-rdfsource/RestRDFSourceView.ts
@@ -1,6 +1,6 @@
-import { Component, ElementRef } from "angular2/core";
-import { CORE_DIRECTIVES } from "angular2/common";
-import { Title } from "angular2/platform/browser";
+import { Component, ElementRef } from "@angular/core";
+import { CORE_DIRECTIVES } from "@angular/common";
+import { Title } from "@angular/platform-browser";
 
 import HighlightDirective from "app/directives/HighlightDirective";
 
