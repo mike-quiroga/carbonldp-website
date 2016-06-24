84c65314a8807322aa254be9c20d9e03c4cdc594
Miguel Aragón <miguel.aragon@base22.com>
2016-06-24 11:50:19 -0500

    Update typescript to @next in order to remove compiler errors

--- typings/typings.d.ts
+++ typings/typings.d.ts
@@ -5,3 +5,18 @@
 
 /// <reference path="./custom/highlightjs/index.d.ts" />
 /// <reference path="./custom/semantic-ui/semantic-ui.d.ts" />
+
+declare module "*.html!" {
+	let value:string;
+	export default value;
+}
+
+declare module "*.css!" {
+	let value:any;
+	export default value;
+}
+
+declare module "*!text" {
+	let value:string;
+	export default value;
+}
