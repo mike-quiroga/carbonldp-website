78205f65e6c659139adb089ab09bf6a701eef13e
Alvaro Contreras <alvaro9210@gmail.com>
2016-07-11 23:44:20 -0500

    Make every component of Explorer implements correctly the ng lifecycle hoooks. Change every selector of Explorer to include a cp- prefix. E.g.(<document-viewer> --> <cp-document-viewer)

--- src/app/app-dev/AppDev.ts
+++ src/app/app-dev/AppDev.ts
@@ -2,7 +2,7 @@ import { Provider } from "@angular/core";
 import { AppDevView } from "app/app-dev/app-dev.view";
 import { AppContextService } from "carbon-panel/my-apps/app-context.service";
 import { ErrorsAreaService } from "carbon-panel/errors-area/errors-area.service";
-import DocumentsResolverService from "app/app-dev/my-apps/app/explorer/document-explorer/documents-resolver.service";
+import { DocumentsResolverService } from "carbon-panel/my-apps/app-content/explorer/document-explorer/documents-resolver.service";
 import JobsService from "./my-apps/app/configuration/app-configuration/job/JobsService";
 import BackupsService from "./my-apps/app/configuration/app-configuration/backup/BackupsService";
 import { SIDEBAR_PROVIDERS } from "./components/sidebar/Sidebar";
