031ffd7a72b3e6f65c2f70aaa17c1b2a15c7493c
Jorge <jorge.espinosa@base22.com>
2016-07-22 20:21:58 -0400

    Add response metadata to token creation response

--- src/main/java/com/carbonldp/authentication/web/TokenAuthenticationRequestHandler.java
+++ src/main/java/com/carbonldp/authentication/web/TokenAuthenticationRequestHandler.java
@@ -3,11 +3,23 @@ package com.carbonldp.authentication.web;
 import com.carbonldp.agents.AgentRepository;
 import com.carbonldp.authentication.Token;
 import com.carbonldp.authentication.token.TokenService;
+import com.carbonldp.ldp.containers.ResourceMetadata;
+import com.carbonldp.ldp.containers.ResourceMetadataDescription;
+import com.carbonldp.ldp.containers.ResponseMetadataDescription;
 import com.carbonldp.ldp.sources.RDFSource;
+import com.carbonldp.ldp.sources.RDFSourceDescription;
 import com.carbonldp.ldp.web.AbstractLDPRequestHandler;
+import com.carbonldp.rdf.RDFBlankNode;
+import com.carbonldp.rdf.RDFResourceDescription;
 import com.carbonldp.spring.TransactionWrapper;
+import com.carbonldp.utils.HTTPUtil;
+import com.carbonldp.utils.ModelUtil;
 import com.carbonldp.web.RequestHandler;
+import org.eclipse.rdf4j.model.BNode;
 import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.Resource;
+import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
@@ -52,7 +64,33 @@ public class TokenAuthenticationRequestHandler extends AbstractLDPRequestHandler
 		}
 		token.getBaseModel().addAll( agentModel );
 
-		return new ResponseEntity<>( token.getBaseModel(), HttpStatus.OK );
+		addResponseMetadata( token, agentModel );
 
+		return new ResponseEntity<>( token.getBaseModel(), HttpStatus.OK );
+	}
+
+	private void addResponseMetadata( Token token, RDFSource agentModel ) {
+		RDFBlankNode responseDescription = getResponseMetadata( token );
+		int eTag = ModelUtil.calculateETag( token );
+		String valueETag = HTTPUtil.formatStrongEtag( eTag );
+
+		BNode bNode = SimpleValueFactory.getInstance().createBNode();
+
+		ResourceMetadata resourceMetadata = new ResourceMetadata( token, bNode );
+		resourceMetadata.addType( ResourceMetadataDescription.Resource.CLASS.getIRI() );
+		resourceMetadata.addType( RDFResourceDescription.Resource.VOLATILE.getIRI() );
+		resourceMetadata.setETag( valueETag );
+		resourceMetadata.setResource( agentModel.getIRI() );
+
+		responseDescription.addResponseMetadata( bNode );
+	}
+
+	private RDFBlankNode getResponseMetadata( Token token ) {
+		ValueFactory valueFactory = SimpleValueFactory.getInstance();
+		BNode bNode = valueFactory.createBNode();
+		RDFBlankNode responseMetadata = new RDFBlankNode( token, bNode, (Resource) null );
+		responseMetadata.add( RDFSourceDescription.Property.TYPE.getIRI(), ResponseMetadataDescription.Resource.CLASS.getIRI() );
+		responseMetadata.add( RDFSourceDescription.Property.TYPE.getIRI(), RDFResourceDescription.Resource.VOLATILE.getIRI() );
+		return responseMetadata;
 	}
 }
