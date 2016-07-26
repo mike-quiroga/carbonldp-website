3484c3c85c708aa84ce341f6891dceae86f21c8c
Nestor Estrada <nestor.estrada.venegas@gmail.com>
2016-07-26 15:17:33 -0500

    Merge branches 'develop' and 'feature/LDP-740-Redirect-a-request-to-platform_agents_me-to-the-agents-IRI' of https://github.com/CarbonLDP/carbon-platform into feature/LDP-740-Redirect-a-request-to-platform_agents_me-to-the-agents-IRI
    Conflicts:
    	src/main/java/com/carbonldp/repository/updates/UpdateAction1o10o0.java

--- src/main/java/com/carbonldp/repository/RepositoriesUpdater.java
+++ src/main/java/com/carbonldp/repository/RepositoriesUpdater.java
@@ -47,6 +47,8 @@ public class RepositoriesUpdater extends AbstractComponent {
 		put( new RepositoryVersion( "1.9.0" ), new UpdateAction1o9o0() );
 		put( new RepositoryVersion( "1.10.0" ), new UpdateAction1o10o0() );
 		put( new RepositoryVersion( "1.11.0" ), new UpdateAction1o11o0() );
+		put( new RepositoryVersion( "1.12.0" ), new UpdateAction1o12o0() );
+		put( new RepositoryVersion( "1.14.0" ), new UpdateAction1o14o0() );
 	}};
 
 	public boolean repositoriesAreUpToDate() {
