1ef79ba299d370e27f8939e4798e38578227cad6
Miguel Aragón <miguel.aragon@base22.com>
2016-10-12 12:17:59 -0500

    Modify Quick Start Guide to reflect the latest installation instructions

--- src/app/website/documentation/quick-start-guide/quick-start-guide.view.ts
+++ src/app/website/documentation/quick-start-guide/quick-start-guide.view.ts
@@ -15,8 +15,14 @@ export class QuickStartGuideView implements AfterViewInit {
 
 	private contentReady:boolean = false;
 
+	private randomString:string;
+
 	constructor( element:ElementRef ) {
 		this.element = element;
+	}
+
+	ngOnInit():void {
+		this.randomString = this.generateRandomString( 32, "aA#" );
 	}
 
 	ngAfterViewInit():void {
@@ -29,5 +35,17 @@ export class QuickStartGuideView implements AfterViewInit {
 		this.$element.find( ".ui.accordion" ).accordion();
 	}
 
+	generateRandomString( length, chars ) {
+		let mask = "";
+		if( chars.indexOf( "a" ) > - 1 ) mask += "abcdefghijklmnopqrstuvwxyz";
+		if( chars.indexOf( "A" ) > - 1 ) mask += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		if( chars.indexOf( "#" ) > - 1 ) mask += "0123456789";
+		if( chars.indexOf( "!" ) > - 1 ) mask += "~`!@#$%^&*()_+-={}[]:\";'<>?,./|\\";
+
+		let result = "";
+		for ( let i = length; i > 0; -- i ) result += mask[ Math.floor( Math.random() * mask.length ) ];
+
+		return result;
+	}
 }
 export default QuickStartGuideView;
