9748cc67b1eb2b33b89289e717d7433a669443fc
Miguel Aragón <miguel.aragon@base22.com>
2016-10-17 10:00:43 -0500

    Remove git submodule linkage for now and add fix for npm postinstall

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,18 +3,20 @@
 cd /usr/src/project
 
 echo "Installing Node dependencies..."
-npm install > /dev/null
+npm install
+# TODO: This shouldn't be needed. Find out why it's not normally being executed after npm install
+npm run-script postinstall
 echo "Node dependencies installed"
 
 echo "Installing jspm dependencies..."
-jspm install --yes > /dev/null
+jspm install --yes
 echo "jspm dependencies installed"
 
-echo "Linking related projects..."
-echo "Linking carbonldp-panel..."
-jspm link carbonldp-panel/dist --yes > /dev/null
-echo "carbonldp-panel linked"
-echo "Related projects linked"
+# echo "Linking related projects..."
+# echo "Linking carbonldp-panel..."
+# jspm link carbonldp-panel/dist --yes > /dev/null
+# echo "carbonldp-panel linked"
+# echo "Related projects linked"
 
 echo "Installing definition files..."
 typings install
