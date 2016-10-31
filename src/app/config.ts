72b845508768ad87dbc78b4cf45e79a63ac5b0ac
Miguel Aragón <miguel.aragon@base22.com>
2016-10-31 13:44:40 -0600

    Remove App Dev in "Not Found" page

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,4 +1,4 @@
-export const CARBON_PROTOCOL:string = "https";
-export const CARBON_DOMAIN:string = "carbonldp.base22.io";
-export const DEBUG:boolean = "true" === "true";
-export const URL_BASE:string = "/src/";
+export const CARBON_PROTOCOL:string = "";
+export const CARBON_DOMAIN:string = "carbonldp.com";
+export const DEBUG:boolean = "false" === "true";
+export const URL_BASE:string = "/";
