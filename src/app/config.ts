ba00c317a9aebc4aac1d5b1dbaf8a6e598615324
Miguel Aragón <miguel.aragon@base22.com>
2016-10-31 13:45:21 -0600

    Remove config.ts from tracked files

--- src/app/config.ts
+++
@@ -1,4 +0,0 @@
-export const CARBON_PROTOCOL:string = "";
-export const CARBON_DOMAIN:string = "carbonldp.com";
-export const DEBUG:boolean = "false" === "true";
-export const URL_BASE:string = "/";
