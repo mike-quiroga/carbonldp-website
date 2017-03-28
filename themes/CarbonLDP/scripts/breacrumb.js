0aac30df7e532a51e77c36a5b4590f7d32d644b7
jgf20 <jimena.guzman.flores@gmail.com>
2017-03-28 16:01:22 -0600

    Fix breadcrumb helper to remove blank slugs

--- themes/CarbonLDP/scripts/breacrumb.js
+++ themes/CarbonLDP/scripts/breacrumb.js
@@ -2,10 +2,15 @@ hexo.extend.helper.register( "breadcrumb", ( page ) => {
 	let breadcrumbElements = {};
 	let path = page.path.replace("/index.html", "");
 	let slugs = path.split( "/" );
-	
+
+	slugs.forEach(( slug, i) => {
+		if(slug === "")
+			slugs.splice(i,1);
+	});
+
 	breadcrumbElements.titles = getTitles(slugs);
 	breadcrumbElements.paths = getPaths(slugs);
-	
+
 	return breadcrumbElements;
 });
 
@@ -48,9 +53,9 @@ function getPaths( slugs ){
 			breadcrumbPath += "/" + slugs[ auxi ];
 			auxi ++;
 		}
-		
+
 		paths.push(breadcrumbPath);
 	});
-	
+
 	return paths;
 }
